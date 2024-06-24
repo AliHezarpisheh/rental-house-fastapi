695aaa3609216e18288566dfdbd1db11525cf2d4
alihezarpisheh <alihezarpisheh@outlook.com>
2024-06-24 13:26:56 +0330

    ♻️ Refactor(logging): Enhance logging configurations

--- config/base.py
+++ config/base.py
@@ -2,11 +2,9 @@
 
 from .database.base import DatabaseConnection
 from .logging.base import LoggingConfig
-from .settings import settings
 
 # Database
 db = DatabaseConnection()
 
 # Logging
-log = LoggingConfig()
-logger = log.get_logger(settings.env)
+logger = LoggingConfig().get_logger()
--- config/logging/base.py
+++ config/logging/base.py
@@ -8,24 +8,43 @@ from typing import Any
 
 from toolkit.parsers import TOMLParser
 
+from ..settings import settings
+
 
 class LoggingConfig:
     """Class for configuring logging settings based on a specified config file."""
 
     def __init__(self, config_path: str = "logging.toml") -> None:
         self._parser = TOMLParser(file_path=config_path)
+        self._logger: logging.Logger | None = None
 
-    def get_logger(self, name: str) -> logging.Logger:
-        """Get a logger with the specified name."""
-        return logging.getLogger(name)
+    def get_logger(self) -> logging.Logger:
+        """Return a logger instance, initializing it if necessary."""
+        if self._logger is None:
+            self.setup()
+        assert self._logger is not None, "Logger setup failed to initialize logger"
+        return self._logger
 
     def setup(self) -> None:
         """Set up the logging configurations."""
         logging_config = self._parser.read()
-        # Check or Create the dirs of log files specified in the config.
+        # Check or create the dirs of log files specified in the config.
         handlers = logging_config.get("handlers", None)
         self.validate_and_create_dirs(handlers=handlers)
+
+        # Determine the appropriate logger configuration based on the environment.
+        env_logger_key = (
+            "development" if settings.env == "development" else "production"
+        )
+        logger_config = logging_config["loggers"].get(env_logger_key)
+
+        if logger_config:
+            logging_config["loggers"][""] = logger_config
+
         logging.config.dictConfig(logging_config)
+
+        # Set the logger object.
+        self._logger = logging.getLogger()
 
     @staticmethod
     def validate_and_create_dirs(handlers: dict[str, dict[str, Any]]) -> list[Path]:
