b63b6632b8f8216a7c82587d34b66a08490be192
alihezarpisheh <alihezarpisheh@outlook.com>
2024-07-17 16:37:38 +0330

    ✨ Feature(auth): Introduce `User` and `Profile` models

---
+++ app/account/auth/models/profile.py
@@ -0,0 +1,62 @@
+"""Module containing model definitions for Profile."""
+
+from __future__ import annotations
+
+from datetime import date
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from toolkit.database.annotations import str255, text
+from toolkit.database.mixins import CommonMixin
+from toolkit.database.orm import Base
+
+if TYPE_CHECKING:
+    from app.account.auth.models.user import User  # pragma: no cover
+else:
+    User = "User"
+
+
+class Profile(CommonMixin, Base):
+    """Model class representing user profile information."""
+
+    __tablename__ = "account__auth__profile"
+
+    # Columns
+    full_name: Mapped[str255] = mapped_column(
+        nullable=False,
+        comment="Full name of the user.",
+    )
+    bio: Mapped[text | None] = mapped_column(
+        nullable=True,
+        comment="Short biography of the user.",
+    )
+    avatar_url: Mapped[str255 | None] = mapped_column(
+        nullable=True,
+        comment="URL to the user's avatar.",
+    )
+    location: Mapped[str255 | None] = mapped_column(
+        nullable=True,
+        comment="Location of the user.",
+    )
+    birthdate: Mapped[date | None] = mapped_column(
+        nullable=True, comment="Birthdate of the user."
+    )
+
+    # Relationships
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("account__auth__user.id"),
+        primary_key=True,
+        nullable=False,
+        comment="Foreign key referencing users table",
+    )
+    user: Mapped[User] = relationship("User", backref="profile")
+
+    def __str__(self) -> str:
+        """Return a string representation of the Profile object."""
+        return f"<Profile(full_name={self.full_name}, user_id={self.user_id})>"
+
+    def __repr__(self) -> str:
+        """Return a human-readable string representation of the Profile object."""
+        return f"Profile({self.full_name}, User ID: {self.user_id})"
---
+++ app/account/auth/models/user.py
@@ -0,0 +1,50 @@
+"""Module containing model definitions for User."""
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from toolkit.database.annotations import str255
+from toolkit.database.mixins import CommonMixin
+from toolkit.database.orm import Base
+
+
+class User(CommonMixin, Base):
+    """Represents a user in the authentication system."""
+
+    __tablename__ = "account__auth__user"
+
+    # Columns
+    username: Mapped[str255] = mapped_column(
+        unique=False,
+        nullable=False,
+        comment="Unique username.",
+    )
+    email: Mapped[str255] = mapped_column(
+        unique=False,
+        nullable=False,
+        comment="Unique email address.",
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        nullable=False, comment="Hashed password."
+    )
+    is_active: Mapped[bool] = mapped_column(
+        default=True,
+        comment="Indicates if the user is active",
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        default=False,
+        comment="Indicates if the user is verified",
+    )
+
+    def __str__(self) -> str:
+        """Return a string representation of the User object."""
+        return (
+            f"<User(username={self.username}, email={self.email}, "
+            f"is_active={self.is_active}, is_verified={self.is_verified})>"
+        )
+
+    def __repr__(self) -> str:
+        """Return a human-readable string representation of the User object."""
+        return (
+            f"User(username={self.username}, email={self.email}, "
+            f"is_active={self.is_active}, is_verified={self.is_verified})"
+        )
