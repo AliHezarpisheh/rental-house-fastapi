54bbe0c144ced125c9453445256ee14d8c91089b
alihezarpisheh <alihezarpisheh@outlook.com>
2024-08-06 18:35:07 +0330

    🚨 Fix(auth): Fix `mypy` errors on explicit imports

--- app/account/auth/models/__init__.py
+++ app/account/auth/models/__init__.py
@@ -1,8 +1,8 @@
-from .activity_log import ActivityLog
-from .permission import Permission
-from .profile import Profile
-from .role import Role
-from .role_permission import RolePermission
-from .token import Token
-from .user import User
-from .user_role import UserRole
+from .activity_log import ActivityLog as ActivityLog
+from .permission import Permission as Permission
+from .profile import Profile as Profile
+from .role import Role as Role
+from .role_permission import RolePermission as RolePermission
+from .token import Token as Token
+from .user import User as User
+from .user_role import UserRole as UserRole
