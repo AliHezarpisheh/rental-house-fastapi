bbd63c316451d659d79f692cfea81dfff0ef4703
alihezarpisheh <alihezarpisheh@outlook.com>
2024-10-05 18:41:01 +0330

    🚧 WIP: Working on user registration path operation function

--- app/account/auth/api/routers/user.py
+++ app/account/auth/api/routers/user.py
@@ -1,8 +1,12 @@
 """Module defines routers related to users."""
 
-from fastapi import APIRouter, status
+from typing import Annotated
 
-from app.account.auth.schemas.user import UserInput
+from fastapi import APIRouter, Depends, status
+
+from app.account.auth.api.dependencies.services import get_user_service
+from app.account.auth.repository.services import UserService
+from app.account.auth.schemas import UserInput
 from toolkit.api.enums import OpenAPITags
 
 router = APIRouter(prefix="/users", tags=[OpenAPITags.USERS])
@@ -11,5 +15,8 @@ router = APIRouter(prefix="/users", tags=[OpenAPITags.USERS])
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register(
     user_input: UserInput,
-) -> None:
+    user_service: Annotated[UserService, Depends(get_user_service)],
+):
     """Register a new user."""  # TODO: Complete the docstring using markdown.
+    user = await user_service.register(user_input=user_input)
+    return user
