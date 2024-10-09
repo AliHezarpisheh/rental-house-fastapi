a43ee006ea046842adf0c49412ad22afb8d87920
ali <alihezarpisheh@outlook.com>
2024-10-09 15:13:46 +0330

    🔧 Refactor(logging): Adjust and enhance logging config
    - Place the logging config in `settings.toml`.
    - Remove `logging.toml`
    - Remove `logging/` dir and place the code in `logging.py`.
    - Refactor and adjust `LoggingConfig` setup.

--- config/logging.py
+++ config/logging.py
@@ -1,5 +1,6 @@
 """Module for configuring logging settings."""
 
+import os
 import logging
 import logging.config
 import logging.handlers
@@ -10,13 +11,38 @@ import coloredlogs
 
 from toolkit.parsers import TOMLParser
 
-from ..settings import settings
+
+class RelativePathFilter:
+    """A logging filter that adds a `relativepath` attribute to log records."""
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        """
+        Modify the log record to include a `relativepath` attribute.
+
+        Parameters
+        ----------
+        record : logging.LogRecord
+            The log record that is being processed by the filter.
+
+        Returns
+        -------
+        bool
+            Returns True to indicate that the log record should be processed.
+
+        Notes
+        -----
+        The `relativepath` is computed based on the `record.pathname` using
+        the current working directory as the starting point.
+        """
+        record.relativepath = os.path.relpath(record.pathname, start=os.getcwd())
+        return True
 
 
 class LoggingConfig:
     """Class for configuring logging settings based on a specified config file."""
 
-    def __init__(self, config_path: str = "logging.toml") -> None:
+    def __init__(self, environment: str = "development", config_path: str = "settings.toml") -> None:
+        self._env = environment
         self._parser = TOMLParser(file_path=config_path)
         self._logger: logging.Logger | None = None
 
@@ -29,27 +55,34 @@ class LoggingConfig:
 
     def setup(self) -> None:
         """Set up the logging configurations."""
-        logging_config = self._parser.read()
+        logging_config = self._parser.read()["logging"]
+
         # Check or create the dirs of log files specified in the config.
         handlers = logging_config.get("handlers", None)
         self.validate_and_create_dirs(handlers=handlers)
 
         # Determine the appropriate logger configuration based on the environment.
-        env_logger_key = (
-            "development" if settings.env == "development" else "production"
-        )
-        logger_config = logging_config["loggers"].get(env_logger_key)
+        logger_config = logging_config["loggers"].get(self._env)
 
         if logger_config:
             logging_config["loggers"][""] = logger_config
 
         logging.config.dictConfig(logging_config)
 
-        # Set up coloredlogs
-        coloredlogs.install(level="DEBUG", logger=self._logger)
-
         # Set the logger object.
         self._logger = logging.getLogger()
+
+        # Add relativepath filter to all the handlers
+        for handler in self._logger.handlers:
+            handler.addFilter(RelativePathFilter())
+
+        # Set up coloredlogs
+        coloredlogs.install(
+            level="DEBUG",
+            logger=self._logger,
+            fmt=logging_config["formatters"]["coreFormatter"]["format"],
+            datefmt=logging_config["formatters"]["coreFormatter"]["datefmt"],
+        )
 
     @staticmethod
     def validate_and_create_dirs(handlers: dict[str, dict[str, Any]]) -> list[Path]:
